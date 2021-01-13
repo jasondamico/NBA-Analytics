0111f36a4b70844a23e89f8373d2b8cb428878a0
jasondamico <damicoj@union.edu>
2021-01-12 21:20:24 -0500

    Change imports to reflect new directory structure

--- data/season_averages/__init__.py
+++ data/season_averages/__init__.py
@@ -7,6 +7,6 @@ currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 
-from .load_data import download_mvp_stats
+from load_data import download_mvp_stats
 
 download_mvp_stats()
