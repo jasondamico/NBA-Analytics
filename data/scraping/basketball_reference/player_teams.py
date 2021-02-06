134f16934a5022af70f9c62dcc0c686a839f0024
jasondamico <damicoj@union.edu>
2021-02-05 19:26:25 -0500

    Add handling of players who played for multiple teams

--- data/scraping/basketball_reference/player_teams.py
+++ data/scraping/basketball_reference/player_teams.py
@@ -33,6 +33,26 @@ def get_player_team_map(season):
         team = tr.find_all("td", {"data-stat":"team_id"})[0].get_text()
         name = tr.find_all("td", {"data-stat":"player"})[0].get_text()
 
-        player_team_map[name] = team
+        # Handling of players who played for multiple teams in the passed season.
+        # NOTE: If a player played for more than one team in the passed season, the team he played the most amount of games for is used in the map.
+        if team == "TOT":
+            multiple_team_player = name
+            most_played_team = {
+                "team": None,
+                "games_played": 0
+            }
+        elif isinstance(most_played_team, dict):    # i.e., if there is still a player who hasn't been added to the map
+            games_played = int(tr.find_all("td", {"data-stat": "g"})[0].get_text())
+
+            if name == multiple_team_player:    # i.e., the player in this loop is the same one that is being handled due to playing for multiple teams
+                if games_played > most_played_team["games_played"]:
+                    most_played_team["team"] = team
+                    most_played_team["games_played"] = games_played
+            else:
+                print(multiple_team_player, most_played_team)
+                player_team_map[multiple_team_player] = most_played_team["team"]
+                most_played_team = None
+        else:
+            player_team_map[name] = team
 
     return player_team_map
